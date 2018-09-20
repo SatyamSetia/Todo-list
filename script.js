a0ed78837b3c1d444d7b10c03b32b8d77af2385c
SatyamSetia <satyam7991@gmail.com>
2018-09-20 11:19:38 +0530

    tasks added to list

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function addTodo() {
-  console.log("add clicked");
+
+  let newtask = document.getElementById('input').value;
+  let tasklist = document.getElementById('tasklist');
+
+  let item = document.createElement('li');
+  item.innerText = newtask;
+  tasklist.appendChild(item);
 }
